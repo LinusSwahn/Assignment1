e7aeddc52e1d6a2b94c59374e3790d23f35983e7
Pontus Pohl <pontuspohl@dhcp-235-059.nomad.chalmers.se>
2015-09-07 16:29:23 +0200

    Commit

--- src/com/bytesizedgames/Dit948Calculator.java
+++ src/com/bytesizedgames/Dit948Calculator.java
@@ -38,16 +38,56 @@ public class Dit948Calculator {
             else System.out.printf("\n");
         }
 
-        System.out.println("Press \"E\" to exit or any other button to continue");
+        System.out.printf("\n\nPress \"E\" to exit or any other button to continue");
         Scanner scanner = new Scanner(System.in);
         String next = scanner.next();
         if(next.equalsIgnoreCase("e")) System.exit(0);
         System.out.println("continuing");
         return true;
     }
+
+    private int p(char operator){
+
+        // check if argument is valid and return correct value
+        switch(operator) {
+            case '+':
+            case '-':
+                return 0;
+
+            case '*':
+            case '/':
+                return 1;
+        }
+        // if incorrect value is passed return -1
+        return -1;
+
+    }
+
+    // TODO: logic ((3 + 5 ∗ 1)/8) ∗ 14 to RPN |||| and RPN -> result;
+
+
+    /*
+    for i=1 to m
+if c_i is an operand: Transfer c_i to output.
+if c_i is a left parentheses: Push c_i to tmp.
+if c_i is a right parentheses: Pop elements from tmp and transfer
+them to output until a left-parentheses
+is met. Pop left-parentheses.
+if c_i is an operator: Let the top tmp element be t. Pop and
+transfer elements from tmp to output
+until:
+p(t) < p(c_i) or
+t is a left-parentheses or
+tmp is empty.
+Push c_i to tmp.
+Transfer the remaining elements in tmp to output.
+     */
+
+
+
     public static void main(String args[]){
 
         Dit948Calculator calculator = new Dit948Calculator();
-
+        System.out.println(calculator.p('*'));
     }
 }
